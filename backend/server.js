f2d9e050820eb9231c3d43d81d0a0cd42656d718
Diyor <diyorkuchmuratov@gmail.com>
2026-08-09 23:47:13 +0500

    fix cars route

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,184 @@
+const mysql = require("mysql2/promise");
+const multer = require("multer");
+const path = require("path");
+const session = require("express-session");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../images"));
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  }
+});
+
+const upload = multer({ storage });
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Davlatbek@2015",
+  database: "bbacars",
+  waitForConnections: true,
+  connectionLimit: 10,
+});
 require("dotenv").config();
+console.log("ADMIN_LOGIN:", process.env.ADMIN_LOGIN);
+console.log("ADMIN_PASSWORD:", process.env.ADMIN_PASSWORD ? "YUKLANDI" : "YUKLANMADI");
 const express = require("express");
 const cors = require("cors");
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const CHAT_ID = process.env.CHAT_ID;
 
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(session({
+    secret: "BBA_CARS_SECRET_2026",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: false
+    }
+}));
+app.post("/admin/login", (req, res) => {
+    console.log("ADMIN LOGIN SO'ROVI KELDI");
+    
+    const { login, password } = req.body;
+
+    if (login === process.env.ADMIN_LOGIN &&
+        password === process.env.ADMIN_PASSWORD) {
+
+        req.session.isAdmin = true;
+
+        return res.json({
+            success: true
+        });
+    }
+
+    res.json({
+        success: false,
+        message: "Login yoki parol noto'g'ri"
+    });
+});
+app.get("/admin/check", (req, res) => {
+    if (req.session.isAdmin) {
+        return res.json({
+            success: true
+        });
+    }
+
+    res.status(401).json({
+        success: false
+    });
+});
+app.use(express.static("../"));
+(async () => {
+  try {
+    const connection = await db.getConnection();
+    console.log("✅ MySQL ulandi!");
+    connection.release();
+  } catch (err) {
+    console.error("❌ MySQL ulanmadi:", err.message);
+  }
+})();
 
 app.get("/", (req, res) => {
     res.send("BBA CARS backend ishlayapti!");
+});
+app.get("/cars", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT * FROM cars ORDER BY id DESC");
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      error: err.message
+    });
+  }
+});
+app.post("/cars", upload.single("image"), async (req, res) => {
+    try {
+
+       const { name, brand, price } = req.body;
+       const image = req.file ? "/images/" + req.file.filename : "";
+
+        await db.query(
+            "INSERT INTO cars (name, brand, price, image) VALUES (?, ?, ?, ?)",
+            [name, brand, price, image]
+        );
+
+        res.json({
+            success: true,
+            message: "Avtomobil muvaffaqiyatli qo'shildi!"
+        });
+
+    } catch (err) {
+        console.error(err);
+
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
+app.delete("/cars/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await db.query("DELETE FROM cars WHERE id = ?", [id]);
+
+    res.json({
+      success: true,
+      message: "Avtomobil o‘chirildi"
+    });
+
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      success: false,
+      error: err.message
+    });
+  }
+});
+app.put("/cars/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, brand, price, image } = req.body;
+
+        const [result] = await db.query(
+    "UPDATE cars SET name = ?, brand = ?, price = ?, image = ? WHERE id = ?",
+    [name, brand, price, image, id]
+);
+
+console.log("EDIT ID:", id);
+console.log("EDIT RESULT:", result);
+
+if (result.affectedRows === 0) {
+    return res.status(404).json({
+        success: false,
+        message: "Bu ID bo‘yicha avtomobil topilmadi"
+    });
+}
+
+res.json({
+    success: true,
+    message: "Avtomobil muvaffaqiyatli tahrirlandi"
+});
+    } catch (err) {
+        console.error(err);
+
+        res.status(500).json({
+            success: false,
+            error: err.message
+        });
+    }
 });
 app.post("/order", async (req, res) => {
     console.log("🔥 /order ISHLADI!");
